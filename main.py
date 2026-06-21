bec82c3b47813d1387ebefb5045a11e3c733a479
PIYUSH TIWARI <piyushayush822114@gmail.com>
2026-06-21 08:11:18 +0530

    Implemented Result to main.py

--- main.py
+++ main.py
@@ -1,10 +1,93 @@
+import json
 from student import Student
 
-s1 = Student(101, "Piyush")
+students = []
 
-s1.add_marks("Math", 90)
-s1.add_marks("Physics", 85)
 
-s1.display()
+def load_data():
+    global students
 
-print("Percentage:", s1.percentage())
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+
+            for item in data:
+                student = Student(
+                    item["roll_no"],
+                    item["name"]
+                )
+
+                student.marks = item["marks"]
+
+                students.append(student)
+
+    except FileNotFoundError:
+        pass
+
+
+def save_data():
+    data = [student.to_dict() for student in students]
+
+    with open("data.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+    print("Data saved successfully.")
+
+
+def add_student():
+    roll_no = int(input("Enter Roll Number: "))
+    name = input("Enter Student Name: ")
+
+    student = Student(roll_no, name)
+
+    n = int(input("Number of Subjects: "))
+
+    for _ in range(n):
+        subject = input("Subject Name: ")
+        marks = float(input("Marks: "))
+        student.add_marks(subject, marks)
+
+    students.append(student)
+
+    print("Student Added Successfully.")
+
+
+
+load_data()
+
+while True:
+    print("\n" + "=" * 40)
+    print("STUDENT RESULT MANAGEMENT SYSTEM")
+    print("=" * 40)
+
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Show Rankings")
+    print("5. Save Data")
+    print("6. Exit")
+
+    choice = input("Enter Choice: ")
+
+    if choice == "1":
+        add_student()
+
+    elif choice == "2":
+        view_students()
+
+    elif choice == "3":
+        search_student()
+
+    elif choice == "4":
+        rank_students()
+
+    elif choice == "5":
+        save_data()
+
+    elif choice == "6":
+        save_data()
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid Choice.")
